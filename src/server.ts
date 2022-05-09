d2f12360c778f0fae90ea51b6a8631e3ea1533d6
FinotelliCarlos <carlos.finotelli@outlook.com>
2022-05-08 23:36:16 -0300

    ..

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,12 @@ import cors from 'cors'
 import { routes } from './routes'
 
 const app = express()
-const port = 3333
+const devPort = 3333
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(process.env.PORT || port, () => {
+app.listen(process.env.PORT || devPort, () => {
   console.log(`Server running! `)
 })
